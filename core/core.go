6867a9b174d1cbc33b7570ff3937f129076ff92a
Raskaro <raskaro@users.noreply.github.com>
2019-08-12 11:50:38 +0200

    Add error-handling for WindowsVersion-stuff

--- core/core.go
+++ core/core.go
@@ -1,11 +1,19 @@
 package core
 
-import "github.com/safing/portbase/modules"
+import (
+	"fmt"
+
+	"github.com/safing/portbase/modules"
+)
 
 var (
 	coreModule = modules.Register("core", nil, startCore, nil, "base", "database", "config", "api", "random")
 )
 
 func startCore() error {
+	if err := startPlatformSpecific(); err != nil {
+		return fmt.Errorf("failed to start plattform-specific stuff: %s", err)
+	}
+
 	return registerDatabases()
 }
---
+++ core/os_default.go
@@ -0,0 +1,8 @@
+// +build !windows
+
+package core
+
+// only return on Fatal error!
+func startPlatformSpecific() error {
+	return nil
+}
