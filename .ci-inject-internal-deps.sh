b5c4d04f6f7f858b1fbf998e865d04e9a49ef78a
Daniel <dhaavi@users.noreply.github.com>
2020-04-08 14:18:51 +0200

    Inject portbase dependency in CI testing

---
+++ .ci-inject-internal-deps.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DEP_FILE="Gopkg.toml"
+
+# remove ignored internal deps
+sed -i '/ignored = \["github.com\/safing\//d' $DEP_FILE
+
+# portbase
+PORTBASE_BRANCH="develop" 
+git branch | grep "* master" >/dev/null
+if [ $? -eq 0 ]; then
+    PORTBASE_BRANCH="master" 
+fi
+echo "
+[[constraint]]
+  name = \"github.com/safing/portbase\"
+  branch = \"${PORTBASE_BRANCH}\"
+" >> $DEP_FILE
