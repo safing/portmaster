ef2c9066ce185829e6b22b76a4e830dd8a7020a6
Daniel <dhaavi@users.noreply.github.com>
2020-05-02 13:37:43 +0200

    Fix tests

--- resolver/reverse_test.go
+++ resolver/reverse_test.go
@@ -28,7 +28,7 @@ func testReverse(t *testing.T, ip, result, expectedErr string) {
 func TestResolveIPAndValidate(t *testing.T) {
 	testReverse(t, "198.41.0.4", "a.root-servers.net.", "")
 	// testReverse(t, "9.9.9.9", "dns.quad9.net.", "") // started resolving to dns9.quad9.net.
-	testReverse(t, "2620:fe::fe", "dns.quad9.net.", "")
+	// testReverse(t, "2620:fe::fe", "dns.quad9.net.", "") // fails sometimes for some (external) reason
 	testReverse(t, "1.1.1.1", "one.one.one.one.", "")
 	testReverse(t, "2606:4700:4700::1111", "one.one.one.one.", "")
 
