e01f185495b54aa8cc375a3bea8dfd41846707d2
Daniel <dhaavi@users.noreply.github.com>
2020-05-27 16:47:33 +0200

    Fix version flag and display

--- core/base/global.go
+++ core/base/global.go
@@ -3,12 +3,13 @@ package base
 import (
 	"errors"
 	"flag"
-
-	"github.com/safing/portbase/modules/subsystems"
+	"fmt"
 
 	"github.com/safing/portbase/api"
 	"github.com/safing/portbase/dataroot"
+	"github.com/safing/portbase/info"
 	"github.com/safing/portbase/modules"
+	"github.com/safing/portbase/modules/subsystems"
 	"github.com/safing/portbase/notifications"
 )
 
@@ -18,16 +19,31 @@ var (
 
 	dataDir     string
 	databaseDir string
+	showVersion bool
 )
 
 func init() {
 	flag.StringVar(&dataDir, "data", "", "set data directory")
 	flag.StringVar(&databaseDir, "db", "", "alias to --data (deprecated)")
+	flag.BoolVar(&showVersion, "version", false, "show version and exit")
 
 	modules.SetGlobalPrepFn(globalPrep)
 }
 
 func globalPrep() error {
+	// check if meta info is ok
+	err := info.CheckVersion()
+	if err != nil {
+		return errors.New("compile error: please compile using the provided build script")
+	}
+
+	// print version
+	if showVersion {
+		fmt.Println(info.FullVersion())
+		return modules.ErrCleanExit
+	}
+
+	// check data root
 	if dataroot.Root() == nil {
 		// initialize data dir
 
