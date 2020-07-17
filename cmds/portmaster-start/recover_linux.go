9eb7195bd862de5c174ee75e854b32f33db9ab0c
Patrick Pacher <patrick.pacher@gmail.com>
2020-07-17 11:21:09 +0200

    Try as hard as possible to remove the nfqueue rules

--- cmds/portmaster-start/recover_linux.go
+++ cmds/portmaster-start/recover_linux.go
@@ -5,17 +5,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var recoverForceFlag bool
 var recoverIPTablesCmd = &cobra.Command{
 	Use:   "recover-iptables",
 	Short: "Removes obsolete IP tables rules in case of an unclean shutdown",
 	RunE: func(*cobra.Command, []string) error {
-		return interception.DeactivateNfqueueFirewall(recoverForceFlag)
+		return interception.DeactivateNfqueueFirewall()
 	},
 	SilenceUsage: true,
 }
 
 func init() {
-	recoverIPTablesCmd.Flags().BoolVarP(&recoverForceFlag, "force", "f", false, "Force removal ignoring errors")
 	rootCmd.AddCommand(recoverIPTablesCmd)
 }
