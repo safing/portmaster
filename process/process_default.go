430031d4acb2f27c60fc4f3ad9dc9e7220f9ec1d
Daniel <dhaavi@users.noreply.github.com>
2020-07-22 11:46:59 +0200

    Remove unused user scope checks

--- process/process_default.go
+++ process/process_default.go
@@ -2,21 +2,6 @@
 
 package process
 
-// IsUser returns whether the process is run by a normal user.
-func (p *Process) IsUser() bool {
-	return true
-}
-
-// IsAdmin returns whether the process is run by an admin user.
-func (p *Process) IsAdmin() bool {
-	return false
-}
-
-// IsSystem returns whether the process is run by the operating system.
-func (p *Process) IsSystem() bool {
-	return false
-}
-
 // IsKernel returns whether the process is the Kernel.
 func (p *Process) IsKernel() bool {
 	return p.Pid == 0
