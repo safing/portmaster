991cebb3d3f478ca89856bc57bef57d2c7c0cc63
Daniel <dhaavi@users.noreply.github.com>
2020-09-25 10:23:59 +0200

    Retry send DNS response without extra section

--- nameserver/response.go
+++ nameserver/response.go
@@ -53,5 +53,14 @@ func writeDNSResponse(w dns.ResponseWriter, m *dns.Msg) (err error) {
 	}()
 
 	err = w.WriteMsg(m)
+	if err == nil {
+		// If we receive an error we might have exceeded the message size with all
+		// our extra information records. Retry again without the extra section.
+		m.Extra = nil
+		noExtraErr := w.WriteMsg(m)
+		if noExtraErr == nil {
+			log.Warningf("nameserver: failed to write dns message with extra section: %s", err)
+		}
+	}
 	return
 }
