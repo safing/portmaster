50d10ffdca97d52ed46478e1f42f1f8114296049
Daniel <dhaavi@users.noreply.github.com>
2021-04-07 16:49:38 +0200

    Bump core version

--- cmds/portmaster-core/main.go
+++ cmds/portmaster-core/main.go
@@ -19,7 +19,7 @@ import (
 
 func main() {
 	// set information
-	info.Set("Portmaster", "0.6.9", "AGPLv3", true)
+	info.Set("Portmaster", "0.6.10", "AGPLv3", true)
 
 	// Configure metrics.
 	metrics.SetNamespace("portmaster")
