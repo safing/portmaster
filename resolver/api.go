317962be5daa5d026bebb9ddc2e09b02744d9323
Daniel <dhaavi@users.noreply.github.com>
2021-04-16 21:32:18 +0200

    Add api endpoint to query the DNS cache database including the memory cache

--- resolver/api.go
+++ resolver/api.go
@@ -1,7 +1,10 @@
 package resolver
 
 import (
+	"net/http"
+
 	"github.com/safing/portbase/api"
+	"github.com/safing/portbase/database/record"
 )
 
 func registerAPI() error {
@@ -21,6 +24,24 @@ func registerAPI() error {
 		StructFunc:  exportDNSResolvers,
 		Name:        "List DNS Resolvers",
 		Description: "List currently configured DNS resolvers and their status.",
+	}); err != nil {
+		return err
+	}
+
+	if err := api.RegisterEndpoint(api.Endpoint{
+		Path: `dns/cache`,
+		Read: api.PermitUser,
+		RecordFunc: func(r *api.Request) (record.Record, error) {
+			return recordDatabase.Get(nameRecordsKeyPrefix + r.URL.Query().Get("q"))
+		},
+		Name:        "Get DNS Record from Cache",
+		Description: "Returns cached dns records from the internal cache.",
+		Parameters: []api.Parameter{{
+			Method:      http.MethodGet,
+			Field:       "q",
+			Value:       "fqdn and query type",
+			Description: "Specify the query like this: `example.com.A`.",
+		}},
 	}); err != nil {
 		return err
 	}
