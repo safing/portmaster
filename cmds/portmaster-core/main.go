fc2375e557a8d773a0008680b23a31eb145d1a52
Daniel <dhaavi@users.noreply.github.com>
2021-05-13 22:17:16 +0200

    Bump versions

--- cmds/portmaster-core/main.go
+++ cmds/portmaster-core/main.go
@@ -19,7 +19,7 @@ import (
 
 func main() {
 	// set information
-	info.Set("Portmaster", "0.6.12", "AGPLv3", true)
+	info.Set("Portmaster", "0.6.13", "AGPLv3", true)
 
 	// Configure metrics.
 	metrics.SetNamespace("portmaster")
--- cmds/portmaster-start/main.go
+++ cmds/portmaster-start/main.go
@@ -77,7 +77,7 @@ func main() {
 	cobra.OnInitialize(initCobra)
 
 	// set meta info
-	info.Set("Portmaster Start", "0.5.2", "AGPLv3", false)
+	info.Set("Portmaster Start", "0.5.3", "AGPLv3", false)
 
 	// catch interrupt for clean shutdown
 	signalCh := make(chan os.Signal, 2)
