266c9505816fd04e456c9ebcd9d19f21a4efe314
Daniel <dhaavi@users.noreply.github.com>
2022-02-14 16:03:10 +0100

    Fix go build flags

--- firewall/interception/nfq/nfq.go
+++ firewall/interception/nfq/nfq.go
@@ -1,4 +1,4 @@
-// go:build linux
+//go:build linux
 
 // Package nfq contains a nfqueue library experiment.
 package nfq
--- netenv/dbus_linux.go
+++ netenv/dbus_linux.go
@@ -1,4 +1,4 @@
-// go:build !server
+//go:build !server
 
 package netenv
 
