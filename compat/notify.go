7d2ae6823722852893772efae536ab009a2e03a6
Daniel <dhaavi@users.noreply.github.com>
2022-03-31 10:02:14 +0200

    Improve prevented bypass attempt notification

--- compat/notify.go
+++ compat/notify.go
@@ -44,7 +44,7 @@ var (
 
 	secureDNSBypassIssue = &appIssue{
 		id:    "compat:secure-dns-bypass-%s",
-		title: "Prevented %s Bypass Attempt",
+		title: "Prevented Bypass Attempt by %s",
 		message: `[APPNAME] attempted to bypass Portmaster's firewall functions by directly using a Secure DNS resolver. If [APPNAME] experiences problems, disable Secure DNS within [APPNAME] to restore functionality. Rest assured that Portmaster already handles Secure DNS for your whole device, including [APPNAME].  
 If bypass prevention for [APPNAME] is disabled in Portmaster, it can no longer protect or filter connections coming from [APPNAME].`,
 		// TODO: Add this when the new docs page is finished:
