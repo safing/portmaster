e5d8e3379006f3a8035ba8f4e2c20f0e372025af
Daniel <dhaavi@users.noreply.github.com>
2022-10-10 16:19:33 +0200

    Bump versions

--- cmds/portmaster-start/main.go
+++ cmds/portmaster-start/main.go
@@ -78,7 +78,7 @@ func main() {
 	cobra.OnInitialize(initCobra)
 
 	// set meta info
-	info.Set("Portmaster Start", "0.9.7", "AGPLv3", false)
+	info.Set("Portmaster Start", "0.9.8", "AGPLv3", false)
 
 	// catch interrupt for clean shutdown
 	signalCh := make(chan os.Signal, 2)
