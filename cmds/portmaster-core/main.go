92ab17d2d49fac050730c464eb1bb02e58e099cd
Daniel <dhaavi@users.noreply.github.com>
2023-08-04 22:22:56 +0200

    Bump core version

--- cmds/portmaster-core/main.go
+++ cmds/portmaster-core/main.go
@@ -24,7 +24,7 @@ import (
 
 func main() {
 	// set information
-	info.Set("Portmaster", "1.3.3", "AGPLv3", true)
+	info.Set("Portmaster", "1.3.4", "AGPLv3", true)
 
 	// Set default log level.
 	log.SetLogLevel(log.WarningLevel)
