b0a75820e79e432eb22e7b2670fd1ffd8c9fb2be
Vladimir Stoilov <vladimir@safing.io>
2024-05-23 15:31:48 +0300

    [windows_kext] Fix the echo is link.ps1

--- windows_kext/release/templates/link.ps1
+++ windows_kext/release/templates/link.ps1
@@ -19,23 +19,23 @@ if(!$?) { Exit $LASTEXITCODE }
 move {{sys_file}} cab\\{{sys_file}}
 move {{pdb_file}} cab\\{{pdb_file}}
 
-echo.
-echo =====
-echo creating .cab ...
+Write-Host
+Write-Host =====
+Write-Host creating .cab ...
 MakeCab /f {{version_file}}.ddf
 
-echo.
-echo =====
-echo cleaning up ...
+Write-Host
+Write-Host =====
+Write-Host cleaning up ...
 del setup.inf
 del setup.rpt
 move disk1\\{{version_file}}.cab {{version_file}}.cab
 rmdir disk1
 
-echo.
-echo =====
-echo YOUR TURN: sign the .cab
-echo use something along the lines of:
-echo.
-echo signtool sign /sha1 C2CBB3A0256A157FEB08B661D72BF490B68724C4 /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a {{version_file}}.cab
-echo.
+Write-Host
+Write-Host =====
+Write-Host YOUR TURN: sign the .cab
+Write-Host use something along the lines of:
+Write-Host
+Write-Host signtool sign /sha1 C2CBB3A0256A157FEB08B661D72BF490B68724C4 /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a {{version_file}}.cab
+Write-Host
