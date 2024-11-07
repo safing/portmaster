af3bb804bf59e897a5ec184b0ecd121e28e26474
Vladimir Stoilov <vladimir@safing.io>
2024-11-07 15:52:26 +0200

    Fix reading only the needed TCP/UDP header bytes (#1730)
    * [windows_kext] Fix reading only the needed TCP/UDP header bytes
    
    * [windows_kext] Disable debug mode
    
    * [windows_kext] Block all fragment packets
    
    * [windows_kext] Improve wording for compiler error

--- windows_kext/driver/src/lib.rs
+++ windows_kext/driver/src/lib.rs
@@ -22,6 +22,11 @@ mod stream_callouts;
 
 use wdk::allocator::WindowsAllocator;
 
+// For consistent behavior during development and production only release mode should be used.
+// Certain behavior of the compiler will change and this can result in errors and different behavior in debug and release mode.
+#[cfg(debug_assertions)]
+compile_error!("Must be built in release mode to ensure consistent behavior and prevent optimization-related issues. Use `cargo build --release`.");
+
 #[cfg(not(test))]
 use core::panic::PanicInfo;
 
