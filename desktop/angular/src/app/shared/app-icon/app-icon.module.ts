0cb17a16bbeef9b60836f076754d2b427001bf39
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2025-04-24 14:36:00 +0300

    Revert "[desktop] Force <img> elements to use Angular HttpClient for image downloads (bypassing WebView)"
    This reverts commit 76289610eeb7fff8143807b76b4e36a3c339826c.

--- desktop/angular/src/app/shared/app-icon/app-icon.module.ts
+++ desktop/angular/src/app/shared/app-icon/app-icon.module.ts
@@ -2,12 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AppIconComponent } from "./app-icon";
 import { AppIconResolver, DefaultIconResolver } from "./app-icon-resolver";
-import { CommonPipesModule } from "../pipes/common-pipes.module";
 
 @NgModule({
   imports: [
-    CommonModule,
-    CommonPipesModule,
+    CommonModule
   ],
   declarations: [
     AppIconComponent,
