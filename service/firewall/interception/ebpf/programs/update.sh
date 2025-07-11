d5698270e92eb6297feffe9febc2ae933fdf72b2
Alexandr Stelnykovych <alexandr.stelnykovycn@ivpn.net>
2025-07-11 19:03:13 +0300

    improvement(ebpf-Linux): enhance eBPF object loading for kernel compatibility and update attach points

--- service/firewall/interception/ebpf/programs/update.sh
+++ service/firewall/interception/ebpf/programs/update.sh
@@ -15,3 +15,8 @@ headers=(
 # Fetch libbpf release and extract the desired headers
 curl -sL "https://github.com/libbpf/libbpf/archive/refs/tags/v${LIBBPF_VERSION}.tar.gz" | \
     tar -xz --xform='s#.*/#bpf/#' "${headers[@]}"
+
+# To generate the vmlinux header file
+# See "Export kernel information" at https://docs.ebpf.io/concepts/core/btf
+#
+#   bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux-x86.h
