b41cded4ccfb375fad6086eca25a8b91e3d96c02
Alexandr Stelnykovych <stenya@gmail.com>
2025-09-03 13:07:12 +0300

    Reorder import statements for better organization and linter compliance

--- base/database/storage/sqlite/prepared.go
+++ base/database/storage/sqlite/prepared.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/safing/portmaster/base/database/record"
-	"github.com/safing/portmaster/base/database/storage"
-	"github.com/safing/portmaster/base/database/storage/sqlite/models"
 	"github.com/safing/structures/dsd"
 	"github.com/stephenafamo/bob"
 	"github.com/stephenafamo/bob/dialect/sqlite/im"
 	"github.com/stephenafamo/bob/expr"
+
+	"github.com/safing/portmaster/base/database/record"
+	"github.com/safing/portmaster/base/database/storage"
+	"github.com/safing/portmaster/base/database/storage/sqlite/models"
 )
 
 var UsePreparedStatements bool = true
